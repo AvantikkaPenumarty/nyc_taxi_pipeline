6536c9dc413af5899f437006a954ce8567477d94
Avantikka Penumarty <38765015+AvantikkaPenumarty@users.noreply.github.com>
2025-06-01 12:47:05 -0700

    Update extract.py
    code change to extract zipfile instead of CSV

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import zipfile
 
-def extract_data(file_path):
-    print("Extracting data...")
-    return pd.read_csv(file_path)
+def extract_data(zip_path):
+    print("Extracting data from ZIP...")
+    with zipfile.ZipFile(zip_path) as z:
+        csv_file = z.namelist()[0]
+        with z.open(csv_file) as f:
+            return pd.read_csv(f)
+
