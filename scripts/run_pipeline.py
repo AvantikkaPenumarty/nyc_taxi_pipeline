7b2c170acdb299a254aa6d172999bd47ab1fd520
Avantikka Penumarty <38765015+AvantikkaPenumarty@users.noreply.github.com>
2025-06-01 12:46:05 -0700

    Update run_pipeline.py
    changing file name

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -3,6 +3,6 @@ from transform import transform_data
 from load import load_data
 
 if __name__ == "__main__":
-    df = extract_data("data/yellow_tripdata_2023-01.csv")
+    df = extract_data("data/yellow_tripdata_filtered.zip")
     df_clean = transform_data(df)
     load_data(df_clean, "output/nyc_taxi.db")
